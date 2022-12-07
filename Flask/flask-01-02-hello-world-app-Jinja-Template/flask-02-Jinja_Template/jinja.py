5c3765d73fda52235feb3d44eea64665f8825b90
John <taskinseref03@gmail.com>
2022-12-07 16:14:40 +0300

    flask-01 and flask-02 added

--- Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ Flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def head():
-    return render_template('index.html', number1 = 1170001, number2 = 2290001)
+    return render_template('index.html', number1 = 117000, number2 = 229000)
 
 @app.route('/sum')
 def number():
-    var1, var2 = 15210, 38960
+    var1, var2 = 15310, 38560
     return render_template('body.html', value1 = var1, value2 = var2, sum = var1+var2)
 
 
